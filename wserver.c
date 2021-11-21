71c358eceff57023c2de05e4e49083eefc463cba
lingxiu <47123365+rocketcrane@users.noreply.github.com>
2021-11-21 15:09:17 -0500

    Update wserver.c
    beginning producer code

--- wserver.c
+++ wserver.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <pthread.h> //pthreads for concurrency
 #include "request.h"
 #include "io_helper.h"
 
@@ -13,7 +14,7 @@ int main(int argc, char *argv[]) {
     int port = 10000;
 	int threads = 1;
 	int buffers = 1;
-	char *schedalg = "FIFO"
+	char *schedalg = "FIFO";
     
     while ((c = getopt(argc, argv, "d:p:t:b:s:")) != -1)
 	switch (c) {
@@ -40,14 +41,45 @@ int main(int argc, char *argv[]) {
     // run out of this directory
     chdir_or_die(root_dir);
 
-    // now, get to work
+	//create_threads(threads);
+	
+    // now, get to work (listen for port)
     int listen_fd = open_listen_fd_or_die(port);
+	
+	//create lock and condition variables
+	pthread_cond_t empty, fill;
+	pthread_mutex_t mutex;
+	
+	int fd_buffer[buffers]; //create circular buffer to store conn_fd (FIFO only)
+	
+	//variables for producer loop
+	int fill = 0;
+	int use = 0;
+	int count = 0;
+	
     while (1) {
-	struct sockaddr_in client_addr;
-	int client_len = sizeof(client_addr);
-	int conn_fd = accept_or_die(listen_fd, (sockaddr_t *) &client_addr, (socklen_t *) &client_len);
-	request_handle(conn_fd);
-	close_or_die(conn_fd);
+		//producer loop
+		for (int i = 0; i < buffers; i++) {
+
+			//if buffer is full then wait for empty signal
+			while (count == buffers) {
+				pthread_cond_wait(&empty, &mutex);
+			}
+			//put:
+			struct sockaddr_in client_addr;
+			int client_len = sizeof(client_addr);
+			int conn_fd = accept_or_die(listen_fd, (sockaddr_t *) &client_addr, (socklen_t *) &client_len);
+			fd_buffer[i] = conn_fd;
+			fill = (fill + 1) % buffers;
+			
+		}
+
+		
+		//request_handle(conn_fd);
+		buffer[fill] = conn_fd;
+
+
+		close_or_die(conn_fd);
     }
     return 0;
 }
